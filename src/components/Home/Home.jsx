4b4dfb5982b4b4f518dba1be7959cc29a72997e0
Andriy_Step <andystep2008@gmail.com>
2023-07-11 12:45:02 +0300

    update text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -17,7 +17,7 @@ function HomePage(){
         <div className="screenRight" id="homepage">
             {showPage ? (
                 <img src={loadingImg} alt="" />
-            ) : <p className="special">Hello there! It is site menu for pizzeria "PizzaLand", made by Andriy Stepaniak. <span className="warn">WARNING: this is pet project, nobody will give you a pizza!</span> I hope you will enjoy this site.
+            ) : <p className="special">Hello there! It is the site menu for "PizzaLand" pizzeria, made by Andriy Stepaniak. <span className="warn">WARNING: this is pet project, nobody will give you a pizza!</span> I hope you will enjoy this site.
         </p>}
         </div>
     )
